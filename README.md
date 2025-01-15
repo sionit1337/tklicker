5661bbc5a01d3ab3f0edda635ed26bcbe72d33f7
sionit_1337 <128901310+sionit1337@users.noreply.github.com>
2025-01-15 11:20:23 +0300

    removed old files

--- README.md
+++
@@ -1,6 +0,0 @@
-# tklicker
-
-simple clicker with save system and gui written in python
-
-## dependencies
-- tkinter 
--- tklicker/consts.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-
-MAX_SLOTS = 20 # maximum slots
-VERSION   = "1.3" # game version
-HERE      = path.dirname(path.abspath(__file__)) # path to the cwd
-RES       = (480, 240) # resolution
